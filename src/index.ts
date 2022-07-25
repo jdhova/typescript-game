3f84068a8b52a249daa0a2cc45111851871dc7b0
Jude Okagu <juud0010@gmail.com>
2022-07-24 22:12:56 -0400

    inputbug

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ let randomNumber4 : number
 let score : number = 1;
 let highScore : number = 0;
 let generateNum : Function
-let myAdd : Function
+let gameOver : Function
 
 let score2 = score > 3 && score < 6 ? score :''
 
@@ -17,15 +17,18 @@ let score2 = score > 3 && score < 6 ? score :''
 
 const scoreBoard = document.querySelector<HTMLParagraphElement>('.score');
 const highScoreBoard = document.querySelector<HTMLParagraphElement>('.highScore');
-// const message1 = document.querySelector<HTMLHeadingElement>('.message1')
-// const messageone  = document.querySelector<HTMLHeadingElement>('.message1');
+
 
 const message  = document.querySelector<HTMLHeadingElement>('.message');
 
 const messageone  = document.querySelector<HTMLHeadingElement>('.messageone');
 
 const restartGame = document.querySelector<HTMLButtonElement>('.restartGame');
+
 const start = document.querySelector<HTMLButtonElement>('.start');
+
+const check = document.querySelector<HTMLButtonElement>('.check');
+
 const restart = document.querySelector<HTMLButtonElement>('.reset');
 const userInput = document.querySelector<HTMLInputElement>('input');
 const img = document.querySelector<HTMLImageElement>("img");
@@ -47,6 +50,37 @@ generateNum = () => {
   
 }
 
+
+ gameOver = () => {
+
+  if (message?.innerText == '' || message?.innerText == null) return
+  message.innerText = 'Game Over...!';
+
+  if (messageone?.innerText == '' || messageone?.innerText == null) return
+  messageone.innerText = 'Game Over...!';
+
+  if (restartGame?.innerText == '' || restartGame?.innerText == null) return
+  restartGame.innerText = 'Play Again...!';
+
+  if (scoreBoard?.innerText == '' || scoreBoard?.innerText == null) return
+  scoreBoard.innerText = String(score);
+
+  if (userInput?.value !== '' || restartGame?.innerText == null) return
+  restartGame.innerText = '';
+
+
+  // restartGame.innerText = 'Play Again';
+  // start.style.display = 'none';
+  // restartGame.style.display = 'block';
+  // scoreBoard.innerText =  score;
+  // userInput.value = '';
+  // userInput.disabled = true;
+  // sound.gameOver.play()
+  // score = 0;
+  // setTimeout(dead, 1000);
+  //setTimeout(hideImage, 8000);
+
+}
 
 
 
@@ -75,3 +109,156 @@ start?.addEventListener('click',  function (){
 
    
 
+  check?.addEventListener('click', function () {
+
+      console.log('home checlk')
+
+
+    if (userInput?.value == '' || userInput?.value == null) return 
+    userInput.value = '';
+    // get the input value here is the bug
+    console.log(answer,'heheh')
+    console.log(userInput.value,'heheh')
+
+    if (message?.innerText == '' || message?.innerText == null) return 
+    message.innerText = '';
+    
+    if (scoreBoard?.innerText == '' || scoreBoard?.innerText == null) return 
+    scoreBoard.innerText = '';
+    
+
+     
+    if (parseInt(userInput.value) === answer && score < 3)  return 
+          message.innerText = 'Correct!'
+          score += 1;
+          scoreBoard.innerText = String(score);
+          generateNum();
+
+    if (parseInt(userInput.value) !== answer && score >  0 && score < 3) 
+          score -= 1;
+          scoreBoard.innerText = String(score);
+          console.log(userInput.value, )
+          message.innerText = 'why Wrong !';
+          generateNum();
+
+
+      if (parseInt(userInput.value) !== answer && score === 0)  
+          gameOver() 
+
+
+
+
+
+        // { 
+
+          
+
+            // return 
+            // message?.innerText = 'Correct!';
+            // score += 1;
+            // scoreBoard.innerText = score;
+            // firstStageSound();
+            // generateNum();
+            // setTimeout(successImage, 1000);
+            // setTimeout(hideImage, 2000);
+           
+    
+            // } else 
+            
+            // if (parseInt(userInput.value) !== answer && score > 0 && score < 3) {   
+            // score -= 1;
+            // scoreBoard.innerText = score;
+            // message.innerText = 'Wrong !';
+            // sound.fail.play();
+            // generateNum();
+            // setTimeout(cry, 1000);
+            // setTimeout(hideImage, 2000);
+            
+
+
+        // } else 
+
+
+        
+    //     if (parseInt(userInput.value) !== answer && score === 0)  
+
+    //     {
+    //         gameOver()    
+    // }  else if 
+    // (parseInt(userInput.value) === answer && score >= 3 && score <= 6)
+
+    // {
+    //     message.innerText = 'Correct!';
+    //         score += 1;
+    //         scoreBoard.innerText = score;
+    //         firstStageSound();
+    //         secondStageSound();
+    //          generateNum2();
+    //         setTimeout(successImage2, 1000);
+    //         setTimeout(hideImage, 2000);
+    //     console.log('activated substracton')
+        
+    // } else if (parseInt(userInput.value) !== answer && score  >= 3 && score <= 6) {   
+    //     score -= 1;
+    //     scoreBoard.innerText = score;
+
+    //     message.innerText = 'Wrong !';
+    //     sound.fail.play();
+    //     generateNum2();
+    //     setTimeout(cry, 1000);
+    //     setTimeout(hideImage, 2000);
+        
+
+
+    // } else if (parseInt(userInput.value) === answer && score >= 6 && score <= 9)
+
+
+    // {
+    //     message.innerText = 'Correct!';
+    //         score += 1;
+    //         scoreBoard.innerText = score;
+    //         firstStageSound();
+    //      generateNum3();
+    //      setTimeout(mag, 1000);
+    //     setTimeout(hideImage, 2000);
+        
+
+        
+    // } else if (parseInt(userInput.value) !== answer && score >= 6 && score <= 9) {   
+    //     score -= 1;
+    //     scoreBoard.innerText = score;
+    //     message.innerText = 'Wrong !';
+    //     sound.fail.play();
+    //     generateNum3();
+    //     setTimeout(cry, 1000);
+    //     setTimeout(hideImage, 2000);
+        
+
+
+    // } else if (parseInt(userInput.value) === answer && score >= 9 && score <= 1200)
+
+
+    // {
+    //     message.innerText = 'Correct!';
+    //         score += 1;
+    //         scoreBoard.innerText = score;
+    //         firstStageSound();
+    //         generateNum4();
+    //         setTimeout(best, 1000);
+    //         setTimeout(hideImage, 2000);
+       
+        
+    // } else if (parseInt(userInput.value) !== answer && score >= 9 && score <= 1200) {   
+    //     score -= 1;
+    //     scoreBoard.innerText = score;
+
+    //     message.innerText = 'Wrong !';
+    //     sound.fail.play();
+    //     generateNum4();
+    //     setTimeout(cry, 1000);
+    //     setTimeout(hideImage, 2000);
+
+    // } 
+
+
+});
